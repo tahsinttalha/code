ea4c1fabd7d34c33463f58809829a90632566dd1
tahsinttalha <tahsinttalha@gmail.com>
2025-07-10 10:24:39 +0600

    routine upload 10 jul

---
+++ learning_cpp/codeforces/1367b.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        int n, last_value, last_index;
+        cin >> n;
+
+        vector <int> array (n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> array[i];
+        }
+
+        
+    }
+}
